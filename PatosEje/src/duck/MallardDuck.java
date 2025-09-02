9cd14e6bb92546a4bb34820995517bcdfe230d18
felkinsio <143757815+felkinsio@users.noreply.github.com>
2025-09-01 22:02:25 -0500

    Update MallardDuck.java

--- PatosEje/src/duck/MallardDuck.java
+++ PatosEje/src/duck/MallardDuck.java
@@ -1,25 +1,17 @@
 package duck;
 
-import fly.FlyBehavior;
 import fly.FlyWithWings;
 import quack.Quack;
-import quack.QuackBehavior;
 
 public class MallardDuck extends Duck {
     
-    FlyBehavior flyBehavior;
-    QuackBehavior quackBehavior;
-
     public MallardDuck (){
         this.flyBehavior = new FlyWithWings();
         this.quackBehavior = new Quack();
-    }
-
-    public updateMallardDuck(MallardDuck mallardDuck, FlyBehavior flyBehavior, QuackBehavior quackBehavior){
-        mallardDuck.flyBehavior = 
     }
     
     public void display(){
         System.out.println("this is a mallard duck");
     }
 }
+
