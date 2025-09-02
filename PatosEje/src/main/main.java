a24c535bb017a2748da08b01b896de220a6ef7d4
felkinsio <143757815+felkinsio@users.noreply.github.com>
2025-09-01 22:03:11 -0500

    Update main.java

--- PatosEje/src/main/main.java
+++ PatosEje/src/main/main.java
@@ -1,8 +1,23 @@
 package main;
 
+import duck.DecoyDuck;
+import duck.Duck;
+import duck.MallardDuck;
+import fly.FlyRocketPowered;
+import quack.Squeak;
+
 public class main {
     
     public static void main(String[] args) {
         
+        Duck duck1 = new MallardDuck();
+        duck1.performQuack();
+
+        duck1.updateDuck(new FlyRocketPowered(), new Squeak());
+        duck1.performQuack();
+
+        Duck duck2 = new DecoyDuck();
+        duck2.performFly();
+    
     }
 }
